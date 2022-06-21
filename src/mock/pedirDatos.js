af2222ca78efd5121bb0ad8e8bfff2d883ee4dec
zarpadoen <yaircarbia420@gmail.com>
2022-06-21 19:36:22 -0300

    Desafio/ItemCollection

--- src/mock/pedirDatos.js
+++
@@ -1,9 +0,0 @@
-import productos from "./data"
-
-export const pedirDatos = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-                resolve(productos)
-        }, 2000)
-    })
-}
